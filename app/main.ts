c7217fc8051e3ff531b8583768705fbfadf1c4bb
Ryan Belev <ryan.belev@gmail.com>
2025-03-01 14:27:58 -0800

    initial cat-file

--- app/main.ts
+++ app/main.ts
@@ -1,24 +1,56 @@
-import * as fs from 'fs';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { promisify } from 'node:util';
+import { unzip } from "node:zlib";
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
     Init = "init",
+    CatFile = "cat-file",
 }
 
 switch (command) {
     case Commands.Init:
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        console.error("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        fs.mkdirSync(".git", { recursive: true });
-        fs.mkdirSync(".git/objects", { recursive: true });
-        fs.mkdirSync(".git/refs", { recursive: true });
-        fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
-        console.log("Initialized git directory");
+        init();
+        break;
+    case Commands.CatFile:
+        await catFile(args);
         break;
     default:
         throw new Error(`Unknown command ${command}`);
+}
+
+async function catFile(args: string[]): Promise<void> {
+   const [,, sha] = args;
+   const path = objectPathFromSha(sha);
+   const fileBuffer = fs.readFileSync(path);
+
+   const fileUnzip = await promisify(unzip)(fileBuffer);
+   const [, content] = splitBlob(fileUnzip);
+   console.log(content);
+}
+
+function objectPathFromSha(sha: string): string {
+    const prefix = sha.slice(0, 2);
+    return path.join('.git', 'objects', prefix, sha);
+}
+
+function splitBlob(file: Buffer): [number, string] {
+   const sizeLineDelimiter = file.indexOf('\0');
+   const blobSizeLine = file.toString('utf8', 0, sizeLineDelimiter);
+   const size = Number.parseInt(blobSizeLine.slice(5), 10);
+
+    const content = file.toString('utf8', sizeLineDelimiter);
+
+    return [size, content];
+}
+
+function init(): void {
+    fs.mkdirSync(".git", { recursive: true });
+    fs.mkdirSync(".git/objects", { recursive: true });
+    fs.mkdirSync(".git/refs", { recursive: true });
+    fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
+    console.log("Initialized git directory");
 }
